aaee96fd2c4789e4047beaab66712f2b041b33bc
franciskuro7 <franciskuro7@gmail.com>
2024-08-01 18:45:20 +0100

    new push

--- login page/login.js
+++ login page/login.js
@@ -9,8 +9,8 @@
 // form.addEventListener('submit', function(event) {
 //   event.preventDefault();
 
-// //   const name = document.getElementById('name').value;
-// //   const email = document.getElementById('email').value;
+//   const name = document.getElementById('name').value;
+//   const email = document.getElementById('email').value;
 
 //   // Validate the input (simplified example)
 //   if (name === '' || email === '') {
@@ -18,8 +18,6 @@
 //     return;
 //   }
 
-//   // Simulate data submission to a server (you would send the data to your server)
-//   // This is just a demonstration; in a real application, use AJAX or fetch to send data.
 //   alert(`Data collected:\nName: ${name}\nEmail: ${email}`);
 
 //   // Reset the form after data collection
